b5bff574aec5bbf8e0b79b68d4af9e41f15fa775
ulumullumak <ulumullumak@protonmail.com>
2020-05-06 12:06:24 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env venv/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'hello world\n'
+
+@app.route('/hello/<username>/')
+def hello_user(username):
+    return ("halli hallo, %s\n" % username)
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
